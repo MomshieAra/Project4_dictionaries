3df9754798910a8fee995ec85c5e527ca0950e82
MomshieAra <marowenanabong@gmail.com>
2022-09-20 23:40:53 -0600

    Create Project5.ts
    First commit

---
+++ Project5.ts
@@ -0,0 +1,13 @@
+var list = {
+    'year': 2018
+    'make': 'nissan
+    'model': 'xterra'
+    'sold": 2022-09-13
+    ).{
+
+        interface vehicle {
+            year: number;
+            masker: string;
+            model: string;
+            sold? : Date 
+            new? : boolean
